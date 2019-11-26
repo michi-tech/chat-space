b9086c5b55ccf7f4bce00c3b934a080fb1624167
michi-tech <michi.swd@gmail.com>
2019-11-26 16:13:46 +0900

    update-deploy.rb

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -52,7 +52,7 @@ $(function() {
   var reloadMessages = function() {
 
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
-      last_message_id = $('.message:last').data('message-id');
+     last_message_id = $('.message:last').data('message-id');
       $.ajax({
         url: "api/messages",
         type: 'get',
